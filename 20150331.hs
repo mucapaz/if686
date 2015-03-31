adc895db24ab3c563c83701ebfc8f4e14ec812f8
pazspm <pazspm@users.noreply.github.com>
2015-03-31 19:42:03 -0300

    Update 20150331.hs

--- 20150331.hs
+++ 20150331.hs
@@ -1,5 +1,5 @@
 -- Aqui começa o trabalho 04
-
+import Data.Char;
 
 --q1
 
@@ -130,6 +130,138 @@ filtroMediana base n
 
 base4 :: [[Int]]
 base4 = [[9,4,5,0,8],[10,3,2,1,7],[9,1,6,3,15],[0,3,8,10,1],[1,16,9,12,7]]
+
+-- Aqui começa os exercicios em sala 
+
+inside :: [(Int,Int,String)] -> Int -> String-> Bool
+inside ((a,b,c):d) i j
+	| a == i  && j == c = True 
+	| d == [] = False
+	| otherwise = inside d i j
+	
+nextNode :: [(Int,Int,String)] -> Int -> String -> Int
+nextNode ((a,b,c):d) e f
+	| (e == a) && (f == c) = b
+	| otherwise = nextNode d e f  
+
+nextAfd :: [(Int,Int,String)] -> Int -> String -> [(Int,Int,String)]
+nextAfd ((a,b,c):d) e f
+	| (e == a) && (f == c) = d
+	| otherwise = [(a,b,c)] ++ (nextAfd d e f) 
+	
+equal :: [Int] -> Int -> Bool
+equal [] i = False
+equal as i
+	| head as == i = True
+	| otherwise = equal (tail as) (i)
+
+afdRun :: String -> [Int] -> [(Int,Int,String)] -> Int -> [Int] -> Bool
+afdRun str est tra at fim
+	| (equal (fim) at) = True
+	| str == "" = False
+	| (inside tra at [head str]) == False = False -- não tem como ele se mover  
+	| (afdRun (tail str) (est) (nextAfd tra at [head str]) (nextNode tra at [head str]) fim) == True = True 
+	| otherwise = afdRun (str) (est) (nextAfd tra at [head str]) at fim 
+		
+
+afd :: String -> [Int] -> [(Int,Int,String)] -> Int -> [Int] -> Bool
+afd str est tra ini fim = afdRun str est tra ini fim  
+
+
+
+-- exerciocio 2
+
+shAux :: String -> Int
+shAux [] = 0
+shAux a = digitToInt (head a) + shAux (tail a)
+
+
+sh :: [String] -> Int
+sh [] = 0
+sh a = (shAux (head a)) + (sh (tail a))
+
+toHex :: Int -> String
+toHex a
+	| a == 0 = ""
+	| otherwise = toHex(div (a) 16) ++ [toUpper(intToDigit(mod (a) 16))] 
+
+somatorioHexadecimal :: [String] -> String
+somatorioHexadecimal as = toHex(sh as)
+
+-- exercicio 3
+
+strToInt :: String -> Int -> Int
+strToInt [] i = i
+strToInt str i = strToInt (tail str) ((i * 16) + digitToInt (head str))
+
+getStack :: String -> Int -> String
+getStack as 0 = []
+getStack as s = (getStack (tail as) (s-1)) ++ [head as] 
+
+getQueue :: String -> Int -> String
+getQueue [] s = []
+getQueue as 0 = [head as] ++ getQueue (tail as) (0)
+getQueue as s = getQueue (tail as) (s-1)
+
+isPalin :: String -> Bool
+isPalin str
+	| (mod tam 2 == 0) = (getStack (str) aux) == (getQueue (str) (aux))   
+	| otherwise = (getStack (str) aux) == (getQueue (str) (aux +1)) 
+	where 
+		tam = size str;
+		aux = div tam 2;
+
+palindromoDecimal :: String -> String
+palindromoDecimal str 
+	| isPalin pro = pro ++ " - PALINDROMO"
+	| otherwise = pro ++ " - NAO-PALINDROMO" 
+	where pro = show (strToInt str 0)
+
+-- exercicio 4
+
+
+type Vector = [Double]
+type Matrix = [Vector]
+
+b4 :: Matrix
+b4 = [[1, 2], [3, 4]]
+
+line :: Matrix -> Int -> Vector
+line m1 0 = head m1
+line m1 i = line (tail m1) (i-1) 
+
+colAux :: Vector -> Int -> Double
+colAux vec 0 = head vec
+colAux vec i = colAux (tail vec) (i-1) 
+
+col :: Matrix -> Int -> Int-> Vector
+col m1 0 i = [colAux (head m1) i]
+col m1 tam i = [colAux (head m1) i] ++ (col (tail m1) (tam-1) i) 
+
+mulAux :: Vector -> Vector -> Double
+mulAux [] [] = 0
+mulAux v1 v2 = (head v1) * (head v2) + mulAux (tail v1) (tail v2)
+
+
+mul :: Matrix -> Matrix -> Int -> Int -> Double
+mul m1 m2 x y = mulAux (line m1 x) (col m2 ((size m2)-1) y)  
+
+mm :: Matrix -> Matrix ->  Vector -> Int -> Int -> Matrix
+mm m1 m2 ent n i
+	| i == n * n = []
+	| y == n - 1 = ([ent ++ [m]] ++ (mm m1 m2 [] n (i+1))) 
+	| otherwise = (mm m1 m2 (ent ++ [m]) n (i+1))
+	where 
+		x = (div i n)
+		y = (i - (x*n))
+		m = mul m1 m2 x y  
+		
+multiplicaMatrizes :: Matrix -> Matrix-> Matrix
+multiplicaMatrizes m1 m2 = mm m1 m2 [] (size m1) 0
+
+
+
+
 
 
 

0a5698e1ef25d26d468d3baca5e5ba867fcddad7
pazspm <pazspm@users.noreply.github.com>
2015-04-16 10:03:24 -0300

    Update 20150416.hs

--- 20150416.hs
+++ 20150416.hs
@@ -27,3 +27,76 @@ listPartitioner :: (Ord t,Num t) => [t] -> ([t] -> [[t]])
 listPartitioner as = lpat as
 
 -- exercicios em aula
+
+f :: Int -> Int -> Int
+f a b = a + b
+
+g a b = (\b a -> f b a)
+
+pri :: [(t,u)] -> [t]
+pri [] = []
+pri ((a,b):as) = [a] ++ pri as 
+
+pri1 :: [(t,u)] -> [t]
+pri1 as =  [fi a | a <- as] 
+	where fi x = fst x
+	
+pri2 :: [(t,u)] -> [t]
+pri2 as =  [ (\x -> fst x) a | a <- as] 
+	
+seg1 :: [[Int]] -> Int -> [[Int]]
+seg1 as n= [a| a<-as, (\x -> (length x) > n) a]
+
+{-
+ter1Aux :: [Int] -> [Bool]
+ter1Aux (a:as) = (\x -> x elem as) as 
+-}
+
+
+rdup :: [Int] -> [Int]
+rdup [] = []
+rdup (a:as)
+	| elem a as = rdup as
+	| otherwise = [a] ++ rdup as
+	
+ter1 :: [[Int]] -> [Int]
+ter1 as = [b | a<-as, b<-a] 
+
+{-
+sumxAux :: Int -> [Int] -> [Int]
+sumxAux x [] = []
+sumxAux x (a:as) = [x + a] ++ sumxAux x as   
+
+sumx :: [Int] -> Int -> [Int]
+sumx as x = (sumxAux x) as
+-}
+
+sx :: [Int] -> Int -> [Int]
+sx as x = map (+x) as
+
+maux :: Int -> Int -> Int
+maux a b
+	| a > b = a
+	|otherwise = b
+
+--mx :: [Int] -> Int
+-- mx as = 
+
+
+-- data Tree t = Nil t | Node t (Tree t) (Tree t)
+
+-- ter1 as = [ [ (\x -> ) |a<-as ] ]
+	
+{-
+pri2 :: [(t,u)] -> [t]
+pri2 as = (\as -> )
+
+
+(\as ) ->  
+
+ma :: (Ord t) => [t] -> t -> [t]
+ma [] x = []
+ma (a:as) x
+	| 
+	
+-}

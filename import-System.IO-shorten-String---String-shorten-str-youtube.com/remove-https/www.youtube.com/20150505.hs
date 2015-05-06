3a2f4950688dc2b72f84bcea7ac3b49ce2ddc954
pazspm <pazspm@users.noreply.github.com>
2015-05-05 23:11:16 -0300

    Create 20150505.hs

---
+++ import-System.IO-shorten-String---String-shorten-str-youtube.com/remove-https/www.youtube.com/20150505.hs
@@ -0,0 +1,33 @@
+-- exercicios de IO
+import System.IO
+
+shorten:: String -> String
+shorten str = "youtube.com/" ++ remove "https://www.youtube.com/watch?v=" str 
+
+remove :: String -> String -> String
+remove [] sb = sb
+remove (a:sa) (b:sb) = remove sa sb
+
+
+
+loop :: Handle -> IO ()
+loop h=
+	do{
+		t <- hIsEOF h;
+		if t			
+		then
+			return ();
+		else do{
+			url <- hGetLine h;
+			putStrLn (shorten url);
+			loop h;
+		}
+
+	}
+
+main:: IO ()
+main = do{
+	handle <- openFile "in" ReadMode;
+	loop handle;
+}
+

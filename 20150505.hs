76933906bed55ee7a6db5b02b2a8b171a6c68184
pazspm <pazspm@users.noreply.github.com>
2015-05-07 13:21:14 -0300

    Update 20150505.hs

--- 20150505.hs
+++ 20150505.hs
@@ -30,3 +30,56 @@ main = do{
 	loop handle;
 }
 
+-- Exercicios em sala 
+
+type Queue = [Int]
+type Stack = [Int]
+
+newtype State t = State {runState :: (Queue, Stack) -> (t, (Queue , Stack))} 
+
+
+instance Monad State where
+	return t = State $ \(st,qu) -> (t,(st,qu))
+	(>>=) (State st) (f) = State $ \(s,q) -> let
+					(x,(newS,newQ)) = st (s,q)
+					(State newState) = f x 
+					in newState (newS,newQ)
+
+pop :: State Int
+pop = State $ (\(a:as,bs) -> (a, (as,bs)))
+
+push :: Int -> State ()
+push x = State $ (\(as,bs) -> ((), (as++[x],bs))) 
+
+tStack :: State ()
+tStack = do{
+	push 10;
+	push 20;
+	pop;
+	push 30;
+	}
+
+tStack2 :: State Int
+tStack2 = do{
+	push 10;
+	push 20;
+	insert 1;
+	insert 2;
+	insert 3;
+	remove;
+	pop;
+
+	}	
+
+main = putStrLn (show (runState tStack ([],[])))
+main2 = putStrLn (show (runState tStack2 ([],[])))
+
+
+insert :: Int -> State ()
+insert x = State $ (\(as,bs) -> ((), (as,x:bs)) ) 
+
+remove :: State Int
+remove = State $ (\(as,x:bs) -> (x,(as,bs)))
+
+
+

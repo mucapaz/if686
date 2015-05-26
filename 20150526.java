0e5921048131fa6498095f6d161739cd909945c6
pazspm <pazspm@users.noreply.github.com>
2015-05-26 18:14:43 -0300

    Update 20150526.java

--- 20150526.java
+++ 20150526.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class PrintNumberThread extends Thread{
-
+	
 	int l1,l2;
 
 	public PrintNumberThread(int l1, int l2){
@@ -54,3 +54,96 @@ public class PrintNumberThread extends Thread{
 }
 
 //e2 
+
+
+package exercicio;
+
+import java.util.Vector;
+
+public class E2Thread extends Thread{
+	
+	int i,j;
+	
+	public E2Thread (int i, int j){
+		this.i = i;
+		this.j = j;
+	}
+	
+	public void loopPrint(){
+		for(int x=i;x<=j;x++){
+			System.out.println(x);
+		}		
+	}
+	
+	public void run (){
+		loopPrint();		
+	}
+	
+	public static void main(String args[]) throws InterruptedException{
+		
+		Vector<E2Thread> vec = new Vector<E2Thread>();
+		
+		for(int x=0;x<10;x++){
+			vec.add(new E2Thread(0, 100000));			
+		}
+		
+		for(int x=0;x<10;x++){
+			vec.elementAt(x).start();
+		}
+		
+		for(int x=0;x<10;x++){
+			vec.elementAt(x).join();			
+		}
+		
+	}
+}
+
+
+// e3 
+
+
+package exercicio;
+
+import java.util.Vector;
+
+public class E2Thread extends Thread{
+	
+	static int i = 0 ,j = 1000000;
+	
+	public E2Thread (){
+		
+	}
+	
+	public void loopPrint(){
+		while(i < j){
+			System.out.println(i);
+			i++;
+		}
+	}
+	
+	public void run (){
+		loopPrint();		
+	}
+	
+	public static void main(String args[]) throws InterruptedException{
+		
+		Vector<E2Thread> vec = new Vector<E2Thread>();
+		
+		for(int x=0;x<10;x++){
+			vec.add(new E2Thread());			
+		}
+		
+		for(int x=0;x<10;x++){
+			vec.elementAt(x).start();
+		}
+		
+		for(int x=0;x<10;x++){
+			vec.elementAt(x).join();			
+		}
+		
+	}
+}
+
+
+//e4 
+
